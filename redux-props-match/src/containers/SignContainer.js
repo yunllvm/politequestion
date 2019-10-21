26a4cddbf9002eb0b57e2ec72f02d73483a79c96
yun <yun@yunui-MacBook-Pro.local>
2019-10-21 16:53:05 +0900

    maybe solved

--- redux-props-match/src/containers/SignContainer.js
+++ redux-props-match/src/containers/SignContainer.js
@@ -5,26 +5,47 @@ import { signchange } from '../actions/sign'
 
 
 
-const SignContainer = ({ props, match }) => {
+// const SignContainer = ({ props, match }) => {
+//     /*
+//     TypeError: Cannot read property 'signstate' of undefined
+//     SignContainer
+//     src/containers/SignContainer.js:10
+//     7 | 
+//     8 | const SignContainer = ({props, match}) => {
+//     9 |     console.log('SignContainer');
+//     > 10 |     console.log(props.signstate.signstate);
+//     11 |     
+//     12 |     const {category} = match.params;
+//     13 |     console.log(category);
+//     */
 
-    /*
-    TypeError: Cannot read property 'signstate' of undefined
-    SignContainer
-    src/containers/SignContainer.js:10
-    7 | 
-    8 | const SignContainer = ({props, match}) => {
-    9 |     console.log('SignContainer');
-    > 10 |     console.log(props.signstate.signstate);
-    11 |     
-    12 |     const {category} = match.params;
-    13 |     console.log(category);
-    */
+//     console.log('SignContainer');
+//     // console.log(props.signstate.signstate);
+//     console.log(match);
+//     console.log(props);
+
+//     const { category } = match.params;
+//     console.log(category);
+
+//     return (
+//         <div>
+//             SignContainer <br />
+//             {/* {props.signstate.signstate.toString()} */}
+//         </div>
+//     )
+// }
+
+
+const SignContainer = (props) => {
+    
 
     console.log('SignContainer');
+    console.log(props);
+
+    const {category} = props;
+    console.log(category);
     console.log(props.signstate.signstate);
 
-    const { category } = match.params;
-    console.log(category);
 
     return (
         <div>
@@ -34,8 +55,12 @@ const SignContainer = ({ props, match }) => {
     )
 }
 
-const mapStateToProps = state => ({
+
+const mapStateToProps = (state, props) => ({
     signstate: state.sign,
+
+    // https://stackoverflow.com/questions/49213602/how-to-get-id-params-to-redux-action-from-react-router
+    category:props.match.params.category
 })
 
 const mapDispatchToProps = dispatch => ({
